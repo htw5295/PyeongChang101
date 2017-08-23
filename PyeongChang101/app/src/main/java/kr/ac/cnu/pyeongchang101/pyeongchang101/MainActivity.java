a70a128fc6d1cbce1579ca3db4c2aac3207d3925
HwangTaeWook <xowktktjd@gmail.com>
2017-08-23 16:05:11 +0900

    01완성, 02추가

---
+++ PyeongChang101/app/src/main/java/kr/ac/cnu/pyeongchang101/pyeongchang101/MainActivity.java
@@ -0,0 +1,24 @@
+package kr.ac.cnu.pyeongchang101.pyeongchang101;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HwangTaeWook on 2017-08-23.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
