1f25191b5a2fe6b659e61922220f3b1c40396177
HwangTaeWook <xowktktjd@gmail.com>
2017-08-24 09:25:13 +0900

    이미지 추가

---
+++ PyeongChang101/app/src/main/java/kr/ac/cnu/pyeongchang101/pyeongchang101/HealingActivity.java
@@ -0,0 +1,20 @@
+package kr.ac.cnu.pyeongchang101.pyeongchang101;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+/**
+ * Created by HwangTaeWook on 2017-08-24.
+ */
+
+public class HealingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_healing);
+    }
+}
